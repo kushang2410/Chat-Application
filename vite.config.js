fac61d8b4636804b38d4679d93a76821d5c3e4f4
Kushang-24 <kushangtanawala24@gmail.com>
2024-12-04 17:51:14 +0530

    Chat-Application

--- vite.config.js
+++ vite.config.js
@@ -3,29 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-
-  server: {
-    port: 5173, 
-    open: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, ''),
-      },
-    },
-  },
-
+  base: '',
   build: {
     outDir: 'dist',
-    sourcemap: true,
   },
-
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
-
-  base: './',
 })
